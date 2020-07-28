34c6780a2fe0097d5cec89b07a4becb93d319dca
mcqueg <garrettmccue@gmail.com>
2020-07-27 19:47:47 -0500

    Initial commit

---
+++ src/Account.java
@@ -0,0 +1,43 @@
+//********************************************************************
+//Garrett McCue 	
+//
+//7/27/20
+//
+//CPSC-501-Lab4Ex2
+//**********************************************************************
+public class Account {
+
+	// -----------------------------------------------------------------
+	// creates new accounts with a constructor while keeping track of
+	// total number of accounts. It then returns the total account number
+	// -----------------------------------------------------------------
+	private static int numAccounts = 0;
+	private String accountNum;
+
+	public static void main(String[] args) {
+
+		Account obj;
+
+		obj = new Account("account1");
+		obj = new Account("account2");
+		obj = new Account("account3");
+		obj = new Account("account4");
+
+		System.out.println("total number of accounts: " + getNumAccounts());
+
+	}
+
+	// constructor to create new account objects as well as increment
+	// numAccounts to keep track of total account count.
+	public Account(String acct) {
+		accountNum = acct;
+		numAccounts++;
+
+	}
+
+	// method to return the total number of accounts
+	public static int getNumAccounts() {
+
+		return numAccounts;
+	}
+}
---
+++ src/Box.java
@@ -0,0 +1,56 @@
+//********************************************************************
+//Garrett McCue 	
+//
+//7/27/20
+//
+//CPSC-501-Lab4Ex1
+//**********************************************************************
+
+public class Box {
+
+	// -----------------------------------------------------------------
+	// A modified version of Box.java from lab3. This uses an overloaded
+	// method for printBox with the parameters char c instead of '*'
+	// -----------------------------------------------------------------
+	private static int size = 0;
+
+	// constructor
+	public Box(int size) {
+	}
+
+	public static void main(String[] args) {
+		Box.size = 5;
+
+		// invokes the fist printBox method
+		Box box1 = new Box(size);
+		box1.printBox();
+
+		// invokes the second printBox and prints out the
+		// character 'c' instead of '*'
+		Box box2 = new Box(size);
+		box2.printBox('c');
+	}
+
+	// print a set of "*" characters w/ size rows and columns
+	public void printBox() {
+
+		for (int i = 0; i < size; i++) {
+			for (int x = 0; x < size; x++) {
+				System.out.print('*');
+			}
+			System.out.println("");
+		}
+	}
+
+	// operates the same as the printBox method above, but
+	// uses a character instead of "*".
+	public void printBox(char c) {
+		for (int i = 0; i < size; i++) {
+			for (int x = 0; x < size; x++) {
+				System.out.print(c);
+			}
+			System.out.println("");
+		}
+	}
+
+}
