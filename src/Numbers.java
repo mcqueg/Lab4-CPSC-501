f71f6d108a9137f8273aa853424278cf1d9bab22
mcqueg <garrettmccue@gmail.com>
2020-07-30 16:39:34 -0500

    Create Numbers.java

---
+++ src/Numbers.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+//********************************************************************
+//Garrett McCue 	
+//
+//7/30/20
+//
+//CPSC-501-Lab4Ex4
+//**********************************************************************
+
+
+public class Numbers {
+	// initialize the array and its length to variables
+	static int nums[] = {78, 22, 56, 99, 12, 14, 17, 15, 1, 144, 37, 23, 47, 88, 3, 19};
+	static int n = nums.length;
+	
+	//-------------------------------------------------------------------
+	//  calls nextLargest to find the next greatest element in an 
+	//  array for each individual element
+	//-------------------------------------------------------------------
+	
+	public static void main(String[] args) {
+		nextLargest(nums);
+	}
+
+	private static void nextLargest(int array[]) {
+		// loop through the array one element at a time
+		for (int i = 0; i < n; i++) {
+			int next = Integer.MAX_VALUE;
+			
+			// second iteration of loops to compare every value 
+			for (int x = 0; x < n; x++) {
+				
+				// assigns next to the next greatest value in the array
+				// for position array[i]. If there is no greater value 
+				// next will be MAX_VALUE. 
+				if (array[x] > array[i] && array[x] < next) {
+					next = array[x];
+
+				}
+			}
+			System.out.println(array[i] + ": " + next);
+		}
+
+	}
+}
